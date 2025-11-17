33e173738099e2bfc5601a365ad97b1589942b55
Brandon Mercier <113701731+bram-odoo@users.noreply.github.com>
2025-11-17 16:19:28 +0100

    [IMP] MouseFollower: publicWidget to Interaction

--- website_airproof/static/src/js/mouse_follower.js
+++ website_airproof/static/src/js/mouse_follower.js
@@ -1,41 +1,48 @@
-/** @odoo-module **/
+import { Interaction } from "@web/public/interaction";
+import { registry } from "@web/core/registry";
 
-import publicWidget from '@web/legacy/js/public/public_widget';
+export class MouseFollower extends Interaction {
+    static selector = "#wrapwrap";
 
-const MouseFollower = publicWidget.Widget.extend({
-    selector: '#wrapwrap',
-    events: { 'mousemove': '_onMouseMove' },
-
-    _onMouseMove: function(ev) {
-        this.mouseX = ev.clientX;
-        this.mouseY = ev.clientY;
-
-        setInterval(() => {
-            this.xp += this.mouseX - this.xp;
-            this.yp += this.mouseY - this.yp;
-
-            this.follower.setAttribute('style', `top: ${this.yp}px; left: ${this.xp}px;`);
-        }, 400);
-    },
-    start: function() {
-        // Initialize starting position
+    setup() {
+        this.mouseX = 0;
+        this.mouseY = 0;
         this.xp = 0;
         this.yp = 0;
-
-        // Create the Follower
-        this.follower = document.createElement('div');
-        this.follower.setAttribute('class', 'x_mouse_follower o_not_editable position-fixed rounded-circle bg-o-color-1 opacity-50 translate-middle pe-none');
-        this.el.querySelector('#top + main').append(this.follower);
-
-        return this._super(...arguments);
-    },
-
-    destroy: function() {
-        this.el.querySelector('.x_mouse_follower').remove();
-
-        return this._super(...arguments);
+        this.follower = null;
+        this._animationId = null;
     }
-});
 
-publicWidget.registry.MouseFollower = MouseFollower;
-export default MouseFollower;
+    start() {
+        this.follower = document.createElement("div");
+        this.follower.className = "x_mouse_follower o_not_editable position-fixed rounded-circle bg-o-color-1 opacity-50 translate-middle pe-none";
+        this.el.append(this.follower);
+    }
+
+    onMouseMove(ev) {
+        this.mouseX = ev.clientX;
+        this.mouseY = ev.clientY;
+        this.xp += (this.mouseX - this.xp) * 0.4;
+        this.yp += (this.mouseY - this.yp) * 0.4;
+
+        this.follower.setAttribute('style', `top: ${this.yp}px; left: ${this.xp}px;`);
+    }
+
+    destroy() {
+        if (this.follower) {
+            this.follower.remove();
+        }
+
+        super.destroy();
+    }
+
+    dynamicContent = {
+        _document: {
+            "t-on-mousemove": this.onMouseMove
+        },
+    };
+}
+
+registry
+    .category("public.interactions")
+    .add("website_airproof.MouseFollower", MouseFollower);
