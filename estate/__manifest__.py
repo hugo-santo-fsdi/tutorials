e8bf13e5b485d8551180ab3d192d84f31b53c54e
Hazem (haabo) <hazem.abulhassan@gmail.com>
2025-11-18 13:16:52 +0100

    [ADD] estate: init

---
+++ estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Real Estate",
+    'summary': """
+        Testing the real estate module
+    """,
+
+    'description': """
+        Testing the description real estate module
+    """,
+    'application': True,
+    'depends': ['base']
+}
