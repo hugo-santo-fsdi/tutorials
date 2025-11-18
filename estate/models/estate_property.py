ba2124fbe14aaf7a2aaa72d4877f57e378ff8557
KarimH537 <karimhassib537@gmail.com>
2025-11-18 15:06:36 +0100

    [ADD] estate: add estate_property fields

---
+++ estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class estateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Real Estate model"
+
+    name = fields.Char(required=True)
+    postcode= fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='Type',
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
+        )
+
+
