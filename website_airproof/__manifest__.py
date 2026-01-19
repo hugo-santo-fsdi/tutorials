554263ca762f176a6a91501e7e3a12a30dbb0937
Brandon Mercier <113701731+bram-odoo@users.noreply.github.com>
2026-01-19 15:59:11 +0100

    [IMP]Pre-commit: XML

--- website_airproof/__manifest__.py
+++ website_airproof/__manifest__.py
@@ -1,72 +1,71 @@
 {
-    'name': 'Airproof Theme',
-    'description': 'Airproof Theme - Drones, modelling, camera',
-    'category': 'Website/Theme',
-    'version': '19.0.1.1',
-    'author': 'PSBE Designers',
-    'license': 'LGPL-3',
-    'depends': ['website_sale', 'website_sale_wishlist', 'website_blog', 'website_mass_mailing'],
-    'data': [
+    "name": "Airproof Theme",
+    "description": "Airproof Theme - Drones, modelling, camera",
+    "category": "Website/Theme",
+    "version": "19.0.1.1",
+    "author": "PSBE Designers",
+    "license": "LGPL-3",
+    "depends": ["website_sale", "website_sale_wishlist", "website_blog", "website_mass_mailing"],
+    "data": [
         # Snippets
-        'views/snippets/options.xml',
-        'views/snippets/s_airproof_carousel.xml',
+        "views/snippets/options.xml",
+        "views/snippets/s_airproof_carousel.xml",
         # Options
-        'data/presets.xml',
-        'data/website.xml',
+        "data/presets.xml",
+        "data/website.xml",
         # Menu
-        'data/menu.xml',
+        "data/menu.xml",
         # Shapes
-        'data/shapes.xml',
+        "data/shapes.xml",
         # Pages
-        'data/pages/home.xml',
-        'data/pages/contact.xml',
+        "data/pages/home.xml",
+        "data/pages/contact.xml",
         # Frontend
-        'views/new_page_template_templates.xml',
-        'views/website_templates.xml',
-        'views/product_tile_templates.xml',
-        'views/website_sale_templates.xml',
-        'views/website_sale_wishlist_templates.xml',
+        "views/new_page_template_templates.xml",
+        "views/website_templates.xml",
+        "views/product_tile_templates.xml",
+        "views/website_sale_templates.xml",
+        "views/website_sale_wishlist_templates.xml",
         # Images
-        'data/images.xml',
+        "data/images.xml",
     ],
-    'assets': {
-        'web._assets_primary_variables': [
-            'website_airproof/static/src/scss/primary_variables.scss',
+    "assets": {
+        "web._assets_primary_variables": [
+            "website_airproof/static/src/scss/primary_variables.scss",
         ],
-        'web._assets_frontend_helpers': [
-            ('prepend', 'website_airproof/static/src/scss/bootstrap_overridden.scss'),
+        "web._assets_frontend_helpers": [
+            ("prepend", "website_airproof/static/src/scss/bootstrap_overridden.scss"),
         ],
-        'web.assets_frontend': [
+        "web.assets_frontend": [
             # SCSS
-            'website_airproof/static/src/scss/base/font.scss',
-            'website_airproof/static/src/scss/components/mouse_follower.scss',
-            'website_airproof/static/src/scss/components/product_card.scss',
-            'website_airproof/static/src/scss/layout/header.scss',
-            'website_airproof/static/src/scss/pages/product_page.scss',
-            'website_airproof/static/src/scss/pages/shop.scss',
-            'website_airproof/static/src/scss/snippets/carousel.scss',
-            'website_airproof/static/src/scss/snippets/newsletter.scss',
-            'website_airproof/static/src/snippets/s_airproof_carousel/000.scss',
+            "website_airproof/static/src/scss/base/font.scss",
+            "website_airproof/static/src/scss/components/mouse_follower.scss",
+            "website_airproof/static/src/scss/components/product_card.scss",
+            "website_airproof/static/src/scss/layout/header.scss",
+            "website_airproof/static/src/scss/pages/product_page.scss",
+            "website_airproof/static/src/scss/pages/shop.scss",
+            "website_airproof/static/src/scss/snippets/carousel.scss",
+            "website_airproof/static/src/scss/snippets/newsletter.scss",
+            "website_airproof/static/src/snippets/s_airproof_carousel/000.scss",
             # JS
-            'website_airproof/static/src/js/mouse_follower.js',
+            "website_airproof/static/src/js/mouse_follower.js",
         ],
-
         # Builder Option Plugins
-        'html_builder.assets': [
-            'website_airproof/static/src/js/html_builder/plugins/background_option/background_shapes_definition.js',
-            'website_airproof/static/src/js/html_builder/plugins/options/header/header_template_option.xml',
-            'website_airproof/static/src/js/html_builder/plugins/options/footer_template_option.xml',
-            'website_airproof/static/src/js/html_builder/plugins/options/mega_menu_option.xml',
-            'website_airproof/static/src/js/html_builder/plugins/options/s_airproof_carousel_option.xml',
-            'website_airproof/static/src/js/html_builder/plugins/options/s_airproof_carousel_option_plugin.js',
+        "html_builder.assets": [
+            "website_airproof/static/src/js/html_builder/plugins/background_option/background_shapes_definition.js",
+            "website_airproof/static/src/js/html_builder/plugins/options/header/header_template_option.xml",
+            "website_airproof/static/src/js/html_builder/plugins/options/footer_template_option.xml",
+            "website_airproof/static/src/js/html_builder/plugins/options/mega_menu_option.xml",
+            "website_airproof/static/src/js/html_builder/plugins/options/s_airproof_carousel_option.xml",
+            "website_airproof/static/src/js/html_builder/plugins/options/s_airproof_carousel_option_plugin.js",
         ],
-        'html_editor.assets_editor': [
-            'website_airproof/static/src/js/html_editor/main/font/color_picker_gradient_tab.js',
+        "html_editor.assets_editor": [
+            "website_airproof/static/src/js/html_editor/main/font/color_picker_gradient_tab.js",
         ],
     },
-    'new_page_templates': {
-        'airproof': {
-            'services': ['s_parallax', 's_airproof_key_benefits_h2', 's_call_to_action', 's_airproof_carousel']
+    "new_page_templates": {
+        "airproof": {
+            "services": ["s_parallax", "s_airproof_key_benefits_h2", "s_call_to_action", "s_airproof_carousel"]
         }
     },
 }
