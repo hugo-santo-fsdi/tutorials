177e27bd910fc43f72df98b1c85c8680f068c2c7
Estagio 02 <estagio.02@termoave.pt>
2026-04-29 11:02:01 +0100

    Add estate property fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,3 +7,22 @@ class EstateProperty(models.Model):
     _description = 'Real Estate Property'
 
     name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ]
+    )
