0a229af559760a8e7eae273e5bc531af1420fa3c
Estagio 02 <estagio.02@termoave.pt>
2026-04-30 11:55:08 +0100

    Add estate dashboard and profile view

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,15 +7,20 @@
     'description': """
         Real estate tutorial module
     """,
-    'version': '0.2',
+    'version': '0.3',
     'application': True,
     'category': 'Tutorials',
     'installable': True,
-    'depends': ['base'],
+    'depends': ['base', 'web'],
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'estate/static/src/dashboard/**/*',
+        ],
+    },
     'author': 'Odoo S.A.',
     'license': 'AGPL-3',
 }
